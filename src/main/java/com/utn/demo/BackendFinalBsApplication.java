d1298708f450000c74872e3855a5838f04efa7db
Julian542 <waltergc248@gmail.com>
2020-06-20 17:27:33 -0300

    Primer commit, inicializacion del proyecto

---
+++ src/main/java/com/utn/demo/BackendFinalBsApplication.java
@@ -0,0 +1,13 @@
+package com.utn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFinalBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFinalBsApplication.class, args);
+	}
+
+}
